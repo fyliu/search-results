2b3fa576ed17e051cd87224002b7d9d9d8c412ec
Fang Yi Liu <fangyiliu@gmail.com>
2020-04-03 16:41:55 -0700

    Initial attempt at header

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -1,9 +1,23 @@
 import React from "react";
+import {Button} from "semantic-ui-react";
 
 function SearchResults() {
   return (
-    <div>
-      <p>SearchResults</p>
+    <>
+    <div className="header">
+      <h1>SearchResults</h1>
+      <div className="navbar">
+        <Button as="a">All</Button>
+        <Button as="a">Movies</Button>
+        <Button as="a">TV Shows</Button>
+        <Button as="a">Games & Apps</Button>
+        <Button as="a">Blog</Button>
+        <Button as="a">Other</Button>
+      </div>
+      <div clasName="liststyle">
+        <Button as="a">tile</Button>
+        <Button as="a">list</Button>
+      </div>
     </div>
       {/*
       <Header>
@@ -14,6 +28,7 @@ function SearchResults() {
       <MovieList />
       <LoadMore />
       */}
+    </>
   );
 }
 
