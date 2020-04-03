2ef159f07a439a4c46b3ceb92fca4a747dab5140
Fang Yi Liu <fangyiliu@gmail.com>
2020-04-03 16:45:13 -0700

    Autoformat with prettier

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -1,24 +1,25 @@
 import React from "react";
-import {Button} from "semantic-ui-react";
+import { createUseStyles } from "react-jss";
+import { Button } from "semantic-ui-react";
 
 function SearchResults() {
   return (
     <>
-    <div className="header">
-      <h1>SearchResults</h1>
-      <div className="navbar">
-        <Button as="a">All</Button>
-        <Button as="a">Movies</Button>
-        <Button as="a">TV Shows</Button>
-        <Button as="a">Games & Apps</Button>
-        <Button as="a">Blog</Button>
-        <Button as="a">Other</Button>
+      <div className="header">
+        <h1>SearchResults</h1>
+        <div className="navbar">
+          <Button as="a">All</Button>
+          <Button as="a">Movies</Button>
+          <Button as="a">TV Shows</Button>
+          <Button as="a">Games & Apps</Button>
+          <Button as="a">Blog</Button>
+          <Button as="a">Other</Button>
+        </div>
+        <div clasName="liststyle">
+          <Button as="a">tile</Button>
+          <Button as="a">list</Button>
+        </div>
       </div>
-      <div clasName="liststyle">
-        <Button as="a">tile</Button>
-        <Button as="a">list</Button>
-      </div>
-    </div>
       {/*
       <Header>
       <Navbar />
