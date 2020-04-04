f01b60b46c16c499a8dd6b8f3be3ceda112fc104
Fang Yi Liu <fangyiliu@gmail.com>
2020-04-04 16:29:04 -0700

    Convert elemnents to semantic ui

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { createUseStyles } from "react-jss";
-import { Button } from "semantic-ui-react";
+import { Header, List } from "semantic-ui-react";
 
 const useStyles = createUseStyles({
   header: {
@@ -17,6 +17,13 @@ const useStyles = createUseStyles({
     height: "135px",
   },
   headRight: {
+    display: "flex",
+    flexDirection: "row",
+    flexWrap: "nowrap",
+    justifyContent: "flex-end",
+    alignItems: "flex-end",
+  },
+  navBar: {
     display: "flex",
     flexDirection: "row",
     flexWrap: "nowrap",
@@ -52,39 +59,73 @@ function SearchResults() {
       <div className={classes.root}>
         <div className={classes.header}>
           <div className={classes.headerTitle}>
-            <h1 className={classes.pageTitle}>Search Results</h1>
+            <Header as="h1" className={classes.pageTitle}>
+              Search Results
+            </Header>
             <div className={classes.headRight}>
-              <div className={classes.navBar}>
-                <Button as="a">All</Button>
-                <Button as="a">Movies</Button>
-                <Button as="a">TV Shows</Button>
-                <Button as="a">Games & Apps</Button>
-                <Button as="a">Blog</Button>
-                <Button as="a">Other</Button>
-              </div>
-              <div className={classes.listStyle}>
-                <Button as="a">tile</Button>
-                <Button as="a">list</Button>
-              </div>
+              <List link className={classes.navBar}>
+                <List.Item as="a" content="All" />
+                <List.Item as="a" icon="film" content="Movies" />
+
+                <List.Item as="a" icon="tv" content="TV Shows" />
+                <List.Item as="a" icon="game" content="Games & Apps" />
+                <List.Item
+                  as="a"
+                  icon="comment alternate outline"
+                  content="Blog"
+                />
+                <List.Item as="a" content="Other" />
+              </List>
+              <List horizontal link className={classes.listStyle}>
+                <List.Item as="a">
+                  <List.Icon name="th" />
+                </List.Item>
+                <List.Item as="a">
+                  <List.Icon name="th list" />
+                </List.Item>
+              </List>
             </div>
           </div>
           <div className={classes.resultsTools}>
-            <Button as="a">Filter</Button>
-            <Button as="a">Popularity</Button>
+            <List horizontal link>
+              <List.Item as="a">
+                <List.Content>Filter</List.Content>
+                <List.Icon name="chevron down" />
+              </List.Item>
+              <List.Item as="a">
+                <List.Content>Popularity</List.Content>
+                <List.Icon name="chevron down" />
+              </List.Item>
+            </List>
           </div>
         </div>
         <div className={classes.resultsFiltersContainer}>
           <div className={classes.resultsFiltersRow}>
-            <div className={classes.resultsFilters}>
-              <Button as="a">Release Year</Button>
-              <Button as="a">Genre</Button>
-              <Button as="a">Rating</Button>
-            </div>
-            <div className={classes.resultsFiltersControls}>
-              <Button as="a">CLEAR FILTERS</Button>
-              <Button as="a">APPLY FILTERS</Button>
-              <Button as="a">X</Button>
-            </div>
+            <List horizontal link className={classes.resultsFilters}>
+              <List.Item as="a">
+                <List.Content>Release Year</List.Content>
+                <List.Icon name="chevron down" />
+              </List.Item>
+              <List.Item as="a">
+                <List.Content>Genre</List.Content>
+                <List.Icon name="chevron down" />
+              </List.Item>
+              <List.Item as="a">
+                <List.Content>Rating</List.Content>
+                <List.Icon name="chevron down" />
+              </List.Item>
+            </List>
+            <List horizontal link className={classes.resultsFiltersControls}>
+              <List.Item as="a">
+                <List.Content>CLEAR FILTERS</List.Content>
+              </List.Item>
+              <List.Item as="a">
+                <List.Content>APPLY FILTERS</List.Content>
+              </List.Item>
+              <List.Item as="a">
+                <List.Icon name="cancel" />
+              </List.Item>
+            </List>
           </div>
         </div>
       </div>
