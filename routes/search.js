f97e05f945f6cd4df62f199e21eff99f4ebf4f01
Fang Yi Liu <fangyiliu@gmail.com>
2020-04-05 12:13:44 -0700

    Set search to movies, returning in json

--- routes/search.js
+++ routes/search.js
@@ -3,9 +3,8 @@ var express = require("express");
 var router = express.Router();
 
 /* GET search results listing. */
-router.get("/", function (req, res, next) {
-  console.log(results);
-  res.send(results);
+router.get("/movies", function (req, res, next) {
+  res.json(results);
 });
 
 module.exports = router;
