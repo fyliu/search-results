5612db8d9057b977ae80c64a13a42b367e785e4e
Fang Yi Liu <fangyiliu@gmail.com>
2020-04-05 12:12:59 -0700

    Hardcode server port to 5000

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 var searchRouter = require("./routes/search");
 
+const port = 5000;
+
 var app = express();
 
 // view engine setup
@@ -39,5 +41,7 @@ app.use(function (err, req, res, next) {
   res.status(err.status || 500);
   res.render("error");
 });
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
 
 module.exports = app;
